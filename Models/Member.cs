f5da6f4fbeea01014c40970843ca41e704cd1f09
Colin Roe <colinkroe@gmail.com>
2019-11-11 16:26:07 +0100

    Initial Commit - Add CRUD for Member

---
+++ Models/Member.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GaaClub.Models
+{
+    public class Member
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Please Enter First Name")]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "Please Enter Last Name")]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        [Display(Name = "Date of Birth")]
+        [DataType(DataType.Date)]
+        public DateTime DateOfBirth { get; set; }
+        public byte Gender { get; set; }
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+    }
+}
---
+++ Models/SeedData.cs
@@ -0,0 +1,64 @@
+﻿using GaaClub.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GaaClub.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new ApplicationDbContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<ApplicationDbContext>>()))
+            {
+                // Look for any movies.
+                if (context.Member.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Member.AddRange(
+                    new Member
+                    {
+                        FirstName = "Colin",
+                        LastName = "Roe",
+                        DateOfBirth = DateTime.Parse("1989-5-10"),
+                        Gender = 1,
+                        Email = "secretary.stuttgart.europe@gaa.ie"
+                    },
+
+                    new Member
+                    {
+                        FirstName = "Dave",
+                        LastName = "Jones",
+                        DateOfBirth = DateTime.Parse("1992-5-10"),
+                        Gender = 1
+                    },
+
+                    new Member
+                    {
+                        FirstName = "Alana",
+                        LastName = "Henderson",
+                        DateOfBirth = DateTime.Parse("1994-5-10"),
+                        Gender = 0
+                    },
+
+                    new Member
+                    {
+                        FirstName = "Felix",
+                        LastName = "Green",
+                        DateOfBirth = DateTime.Parse("1991-6-21"),
+                        Gender = 1,
+                        Email = "chairperson.stuttgart.europe@gaa.ie"
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
