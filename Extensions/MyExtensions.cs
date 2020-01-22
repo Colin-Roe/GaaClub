fcdc6dd30f8ee4e149b5e43d93c2222e40a4bc9b
Colin Roe <colinkroe@gmail.com>
2020-01-22 12:44:44 +0100

    Update Member Details View

---
+++ Extensions/MyExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace GaaClub.Helpers
+{
+    public static class MyExtensions
+    {
+        public static string YesNo(this bool value)
+        {
+            return value ? "Yes" : "No";
+        }
+
+        public static string YesNo(this byte value)
+        {
+            if (value == 0)
+                return "No";
+            else
+                return "Yes";
+        }
+    }
+}
